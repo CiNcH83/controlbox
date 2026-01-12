79c654d1d8ed75cae58718d9a331731feafa4af3
Klaus Jaroslawsky <Klaus.Jaroslawsky@so-oftware.de>
2026-01-12 09:50:35 +0100

    frontend project files corrected

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //root: "./frontend",
   publicDir: "public",
-  //base: "./",
   build: {
     outDir: "./dist/",
     emptyOutDir: true,
